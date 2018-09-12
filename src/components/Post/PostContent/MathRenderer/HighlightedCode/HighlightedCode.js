430753ecdb6c24a52c268a9d11001a0ca2d7aa76
Srikavin Ramkumar <9142257+srikavin@users.noreply.github.com>
2018-09-12 19:05:01 -0400

    Load syntax highlighting packages dynamically

--- src/components/Post/PostContent/MathRenderer/HighlightedCode/HighlightedCode.js
+++ src/components/Post/PostContent/MathRenderer/HighlightedCode/HighlightedCode.js
@@ -1,16 +1,32 @@
 import * as React from 'react';
-import Loadable from 'react-loadable';
-import {DynamicLoading} from '../../../../DynamicLoading/DynamicLoading';
 import {docco} from 'react-syntax-highlighter/styles/hljs';
-
-const SyntaxHighlighter = Loadable({
-    loader: () => import(/* webpackChunkName: "code-highlighter" */'react-syntax-highlighter'),
-    loading: DynamicLoading
-});
+import SyntaxHighlighter, {registerLanguage} from 'react-syntax-highlighter/light';
 
 
 class HighlightedCode extends React.Component {
     displayName = 'CodeBlock';
+
+    constructor(props) {
+        super(props);
+        this.loadLanguage();
+    }
+
+
+    componentDidUpdate(prevProps) {
+        if (this.props.language !== prevProps.language) {
+            this.loadLanguage();
+        }
+    }
+
+    loadLanguage() {
+        if (this.props.language && this.props.language !== null) {
+            let lang = this.props.language.toLowerCase();
+            import('react-syntax-highlighter/languages/hljs/' + lang).then(e => {
+                registerLanguage(lang, e.default);
+                this.forceUpdate();
+            });
+        }
+    }
 
     render() {
         return (
